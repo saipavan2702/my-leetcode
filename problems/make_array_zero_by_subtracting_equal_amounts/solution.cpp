b8adc86ace66cc95257598d86bd4ba331e581873
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2022-08-03 03:25:00 +0000

    Sync LeetCode submission - Make Array Zero by Subtracting Equal Amounts (cpp)

---
+++ problems/make_array_zero_by_subtracting_equal_amounts/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+   
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int>st;
+        for(auto &x:nums)
+        if(x!=0)
+        st.insert(x);
+        
+        return st.size();
+    }
+};
