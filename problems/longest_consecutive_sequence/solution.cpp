1d7b305edadc59b196ffae9a9d838971496d8f46
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2022-08-25 19:30:23 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (cpp)

--- problems/longest_consecutive_sequence/solution.cpp
+++ problems/longest_consecutive_sequence/solution.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-         if(nums.size()==0)
-        return 0;
+    int i,n,res=1;
+    int maxx=0;
+    set<int>st;
+    for(auto i:nums)
+    {
+        st.insert(i);
+    }
+    nums.clear();
+    for(auto i:st)
+    {
+      nums.push_back(i);
+    }
+    n=nums.size();
+    if(n==0)
+    return 0;
         
-        unordered_set<int>st;
-        for(auto x:nums)
-        {
-            st.insert(x);
-        }
-        nums.clear();
-        for(int x:st)
-        {
-            nums.push_back(x);
-        }
-        int n,res=INT_MIN,len=1;
-        n=nums.size();
-        if(n==1)
-        return 1;
-        int x=0,y=1;
-        sort(nums.begin(),nums.end());
-        while(y<n)
-        {
-            if(nums[y]-nums[x]!=len)
-            {
-                res=max(res,len);
-                x=y;
-                len=1;
-            }
-            else if(nums[y]-nums[x]==len)
-            len++;
-            
-            y++;
-        }
-        if(nums[n-1]-nums[n-2]==1)
-        res=max(res,len);
+    if(n==1)
+    return 1;
         
-        return res;
+    for(i=0;i<n-1;i++)
+    {
+        if(nums[i+1]-nums[i]==1)
+        {
+           res++;
+        }
+        else
+        {
+            maxx=max(res,maxx);
+            res=1;
+        }
+        
+    }
+    if(nums[n-1]-nums[n-2]==1)
+    maxx=max(res,maxx);
+        
+    return maxx;
     }
 };
