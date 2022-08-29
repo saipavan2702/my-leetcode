fc64aa64a7d9b963d7859c1963cdb795b137c6a6
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2022-08-29 05:28:49 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- problems/number_of_islands/solution.cpp
+++ problems/number_of_islands/solution.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid,int x,int y)
-{
-    int n,p;n=grid.size();
-    p=grid[0].size();
-    if(x<0 or y<0 or x>n-1 or y>p-1 or grid[x][y]=='0')
-    return;
-    
-    
-    grid[x][y]='0';
-    dfs(grid,x+1,y);
-    dfs(grid,x,y-1);
-    dfs(grid,x-1,y);
-    dfs(grid,x,y+1);
-}
-    int numIslands(vector<vector<char>>& grid) {
-        int n,p,cnt=0;n=grid.size();
-    p=grid[0].size();
-    for(int i=0;i<n;i++)
+    void dfs(int x,int y,vector<vector<char>>& grid)
     {
-        for(int j=0;j<p;j++)
+        int n,p;
+        n=grid.size();
+        p=grid[0].size();
+        
+        if(x<0 or y<0 or x>=n or y>=p or grid[x][y]=='0')
+        return;
+        
+        grid[x][y]='0';
+        dfs(x,y-1,grid);
+        dfs(x+1,y,grid);
+        dfs(x-1,y,grid);
+        dfs(x,y+1,grid);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n,p,cnt=0;
+        n=grid.size();
+        p=grid[0].size();
+        for(int i=0;i<n;i++)
         {
-            if(grid[i][j]=='1')
+            for(int j=0;j<p;j++)
             {
-                dfs(grid,i,j);
-                cnt++;
+                if(grid[i][j]=='1')
+                {
+                 dfs(i,j,grid);
+                 cnt++;
+                }
             }
         }
-    }
-    return cnt;
+        return cnt;
     }
 };
