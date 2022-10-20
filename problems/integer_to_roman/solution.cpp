6c22b962d44faf042c4b63926b1e5af43828fa96
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2022-10-20 04:10:07 +0000

    Sync LeetCode submission - Integer to Roman (cpp)

---
+++ problems/integer_to_roman/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        int ret[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string dict[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        string ans="";
+        
+        for(int i=0;i<13;i++)
+        {
+            while(ret[i]<=num)
+            {
+                ans+=dict[i];
+                num-=ret[i];
+            }
+        }
+        return ans;
+    }
+};
