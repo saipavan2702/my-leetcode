a0dbad11b9521eefcb9713644d4c7f6644238a0b
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2022-10-31 13:46:30 +0000

    Sync LeetCode submission - Toeplitz Matrix (cpp)

--- problems/toeplitz_matrix/solution.cpp
+++ problems/toeplitz_matrix/solution.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        int n=matrix.size(),m=matrix[0].size();
-    int i, j;
-    for (i = 0; i <n-1; i++) {
-        for (j =0; j<m-1; j++) {
-            if (matrix[i][j] != matrix[i+1][j+1]) {
-                return false;
+    bool isToeplitzMatrix(vector<vector<int>>&t) {
+        int n,p;
+        n=t.size();
+        p=t[0].size();
+        
+        unordered_map<int,int>freq;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<p;j++)
+            {
+               if(freq.count(i-j))
+               {
+                   if(freq[i-j]!=t[i][j])
+                       return 0;
+               }
+               freq[i-j]=t[i][j];
             }
         }
-    }
-    return true;
+        return 1;
     }
 };
