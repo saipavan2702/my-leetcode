86c67bbcac023e88fbf81fef10be439763b68b69
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2022-11-10 13:27:30 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates In String (cpp)

---
+++ problems/remove_all_adjacent_duplicates_in_string/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>st;
+        for(auto &c:s)
+        {
+            if(st.empty())
+            st.push(c);
+            else if(st.top()==c and !st.empty())
+            st.pop();
+            else
+            st.push(c);
+        }
+        
+        string ans="";
+        while(!st.empty())
+        {
+            ans=st.top()+ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
