4b4f77cb676331c8e34faaa9b4de2f0f293fad4b
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2022-12-19 06:44:45 +0000

    Sync LeetCode submission - Find if Path Exists in Graph (cpp)

--- problems/find_if_path_exists_in_graph/solution.cpp
+++ problems/find_if_path_exists_in_graph/solution.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>&edges, int s, int d) {
-        if(s==d)
-        return 1;
-        
-        vector<int>adj[n+1];
-        for(auto x:edges)
+    bool validPath(int n, vector<vector<int>>& edges, int src, int dest) {
+        vector<int>adj[n];
+        for(auto e:edges)
         {
-         adj[x[0]].push_back(x[1]);
-         adj[x[1]].push_back(x[0]);
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
-        
         queue<int>q;
-        vector<bool>vis(n+1,0);
-        vis[s]=1;
-        q.push(s);
-        
+        q.push(src);
+
+        vector<int>vis(n);
+        vis[src]=1;
         while(!q.empty())
         {
-            int r=q.front();
-            q.pop();
-            
-            if(r==d)
-            return 1;
-            
-            for(auto x:adj[r])
-            {
-                if(!vis[x])
-                {
-                    vis[x]=1;
-                    q.push(x);
-                }
-            }
+           int curr=q.front();
+           q.pop();
+           if(curr==dest)
+               return 1;
+
+           for(auto u:adj[curr])
+           {
+               if(!vis[u])
+               vis[u]=1,q.push(u);
+           }
         }
         return 0;
     }
