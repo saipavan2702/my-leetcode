1da7740e7961e9b9420db73892b91d0f3dcc9609
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-01-11 11:15:57 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int>dp(n+1);
-        if(n==0)
+        if(!n)
         return 0;
+        
+        int dp[n+1];
+        memset(dp,0,sizeof(dp));
         dp[0]=0;
         dp[1]=1;
         for(int i=2;i<=n;i++)
-        {
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+        dp[i]=dp[i-1]+dp[i-2];
+
         return dp[n];
     }
 };
