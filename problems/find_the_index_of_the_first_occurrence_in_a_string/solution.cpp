86c5fa4cd212eeeade9712d168130d254bc027c3
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-03-03 06:40:48 +0000

    Sync LeetCode submission - Find the Index of the First Occurrence in a String (cpp)

--- problems/find_the_index_of_the_first_occurrence_in_a_string/solution.cpp
+++ problems/find_the_index_of_the_first_occurrence_in_a_string/solution.cpp
@@ -1,61 +1,16 @@
 class Solution {
 public:
-    void pref(string need,int lps[])
-    {
-        int l=0;
-        lps[0]=0;
-        for(int i=1;i<need.size();i++)
-        {
-            if(need[l]==need[i])
-            {
-                l++;
-                lps[i]=l;
-            }
-            else
-            {
-                if(l!=0)
-                {
-                    l=lps[l-1];
-                    i--;
-                }
-                else
-                {lps[i]=0;}
-            }
-        }
-    }
     int strStr(string hay, string need) {
-       int n,m,r=0,l=0;
-        n=hay.size();
-        if(n==0)
-        return 0;
-        m=need.size();
-        int lps[m];
-        pref(need,lps);
-        int cnt=0;
-        while(r<n)
-        {
-            if(hay[r]==need[l])
-            {
-                r++;
-                l++;
+        int j=0;;
+        int p=need.size();
+
+        while(j<hay.size()){
+            if(hay[j]==need[0]){
+                if(hay.substr(j,p)==need)
+                return j;
             }
-            if(l==m)
-            {
-                return r-l;
-                //ans[cnt]=r-l
-                //cnt++
-                //l=lps[l-1]
-            }
-            else if(r<n and hay[r]!=need[l])
-            {
-                if(l!=0)
-                l=lps[l-1];
-                else
-                r++;    
-            }
-                
+            j++;
         }
         return -1;
-        
     }
 };
