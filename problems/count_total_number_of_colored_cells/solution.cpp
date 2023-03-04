4016942f57a6b4fbbef6a2bff70d95a71545a0aa
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-03-04 14:47:42 +0000

    Sync LeetCode submission - Count Total Number of Colored Cells (cpp)

---
+++ problems/count_total_number_of_colored_cells/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long ans=1;
+        
+        for(int i=1;i<n;i++){
+            ans+=(4*i);
+        }
+        return ans;
+        
+    }
+};
