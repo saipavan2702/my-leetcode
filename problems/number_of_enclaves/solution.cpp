b0ee546a724331ca6049aaef74c4099adeab0025
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-04-07 06:57:53 +0000

    Sync LeetCode submission - Number of Enclaves (cpp)

--- problems/number_of_enclaves/solution.cpp
+++ problems/number_of_enclaves/solution.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid,int x,int y)
-    {
-         int n,p;
-        n=grid.size();
-        p=grid[0].size();
-        if(x<0 or x>=n or y<0 or y>=p or grid[x][y]==0)
-        return;
-        
-        grid[x][y]=0;
-        dfs(grid,x-1,y);
-        dfs(grid,x,y-1);
-        dfs(grid,x+1,y);
-        dfs(grid,x,y+1);
-    }
     int numEnclaves(vector<vector<int>>& grid) {
-        int n,p;
-        n=grid.size();
-        p=grid[0].size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<p;j++)
-            {
-                if(i==0 or j==0 or i==n-1 or j==p-1 and grid[i][j]==1)
-                {
-                    dfs(grid,i,j);
+        int n=grid.size();
+        int p=grid[0].size();
+
+        function<void(int,int)>dfs=[&](int x, int y){
+            if(x<0 || x>=n || y<0 || y>=p)
+            return;
+
+            if(grid[x][y]==0)
+            return;
+
+            grid[x][y]=0;
+            dfs(x-1,y);
+            dfs(x+1,y);
+            dfs(x,y-1);
+            dfs(x,y+1);
+        };
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<p;j++){
+                if((i==0 || i==n-1 || j==0 || j==p-1) and grid[i][j]==1){
+                    dfs(i,j);
                 }
             }
         }
+
         int cnt=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<p;j++)
-            {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<p;j++){
                 if(grid[i][j]==1)
-                {
-                    cnt++;
-                }
+                cnt++;
             }
         }
         return cnt;
