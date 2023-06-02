973bdce19c1032575e3c49a37aea3d211d0f6074
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-06-02 03:02:40 +0000

    Sync LeetCode submission - Minimum Amount of Time to Fill Cups (cpp)

--- problems/minimum_amount_of_time_to_fill_cups/solution.cpp
+++ problems/minimum_amount_of_time_to_fill_cups/solution.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
-    int fillCups(vector<int>& a) {
-        sort(a.begin(),a.end());
-        int r,q,sum=0,cnt=0;
-        for(auto x:a)
-        {
-            if(x==0)
-            {
-                cnt++;
-            }
-            sum+=x;
-        }
-        if(cnt==2)
-        return sum;
-        else if(a[0]+a[1]>a[2])
-        {r=sum%2;
-        q=sum/2;
-        return q+r;}
-        else
-        return a[2];
-        
+    int fillCups(vector<int>& A) {
+        sort(A.begin(),A.end());
+        int s=accumulate(A.begin(),A.end(),0);
+        int have=ceil(s*1.0/2);
+        return max(have,A[2]);
     }
 };
