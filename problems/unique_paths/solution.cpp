6c43030519f954f34f08b32f39ac3ea47b0963c0
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-06-03 08:18:47 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- problems/unique_paths/solution.cpp
+++ problems/unique_paths/solution.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    int dfs(int m,int n,int st,int end,vector<vector<int>>&dp)
-    {
-        if(st==m-1 and end==n-1)
-         return 1;
-        if(st>=m or end>=n)
-         return 0;
-        if(dp[st][end]!=-1)
-        return dp[st][end];
-        
-        return dp[st][end]=dfs(m,n,st+1,end,dp)+dfs(m,n,st,end+1,dp);
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        int cnt=dfs(m,n,0,0,dp);
-        return cnt;
+     vector<vector<int>>dp(m,vector<int>(n,-1));
+
+     function<int(int,int)>dfs=[&](int x, int y){
+        if(x<0 || x>=m || y<0 || y>=n)
+        return 0;
+
+        if(x==m-1 and y==n-1)
+        return 1;
+
+        if(dp[x][y]!=-1)
+        return dp[x][y];
+
+        int op1=dfs(x+1,y);
+        int op2=dfs(x,y+1);
+
+        return dp[x][y]=op1+op2;
+     };
+
+     return dfs(0,0);
     }
 };
