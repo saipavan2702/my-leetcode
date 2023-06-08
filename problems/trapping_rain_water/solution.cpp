a916ad7ad81031427d12e9022b2b89b36fe6f876
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-06-08 11:13:56 +0000

    Sync LeetCode submission - Trapping Rain Water (cpp)

--- problems/trapping_rain_water/solution.cpp
+++ problems/trapping_rain_water/solution.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
-    int trap(vector<int>& ht) {
-        int n,i=0;
-        n=ht.size();
-        int right=0,left=0,ans=0,j=n-1;
-        while(i<j)
-        {
-            if(ht[i]>=ht[j])
-            {
-             if(right<ht[j])
-             right=ht[j];
-             else
-             ans+=(right-ht[j]);
+    int trap(vector<int>& h) {
+        int n=h.size();
+
+        int depth(0);
+        stack<int>st;
+        vector<int>left(n,-1),right(n,-1);
+
+        for(int i=0;i<n;i++){
+            if(st.empty())
+            st.push(h[i]);
+            else{
+                while(!st.empty() && st.top()<=h[i]){
+                    st.pop();
+                }
+
+                if(!st.empty())
+                left[i]=st.top();
                 
-             j--;
-            }
-            else
-            {
-                if(left<ht[i])
-                left=ht[i];
-                else
-                ans+=(left-ht[i]);
-                
-                i++;
+                if(st.empty() || h[i]>st.top())
+                st.push(h[i]);
             }
         }
-        return ans;
+
+        while(st.size())st.pop();
+
+        for(int i=n-1;i>=0;i--){
+            if(st.empty())
+            st.push(h[i]);
+            else{
+                while(!st.empty() && st.top()<=h[i])
+                st.pop();
+
+                if(!st.empty())
+                right[i]=st.top();
+
+                if(st.empty() || h[i]>st.top())
+                st.push(h[i]);
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(left[i]!=-1 && right[i]!=-1)
+            depth+=min(left[i]-h[i],right[i]-h[i]);
+        }
+        return depth;
+
     }
 };
