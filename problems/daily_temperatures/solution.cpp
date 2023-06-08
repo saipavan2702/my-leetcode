4ceb546c2be47c7a5b5a3007205aaa0e11c45960
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-06-08 09:17:19 +0000

    Sync LeetCode submission - Daily Temperatures (cpp)

--- problems/daily_temperatures/solution.cpp
+++ problems/daily_temperatures/solution.cpp
@@ -7,13 +7,30 @@ public:
         stack<pair<int,int>>st;
         vector<int>ret(n);
 
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() and t[i]>st.top().first)
-            ret[st.top().second]=i-st.top().second,st.pop();
+        // for(int i=0;i<n;i++)
+        // {
+        //     while(!st.empty() and t[i]>st.top().first)
+        //     ret[st.top().second]=i-st.top().second,st.pop();
             
-            st.push({t[i],i});
-        }
-        return ret;
+        //     st.push({t[i],i});
+        // }
+
+       for(int i=n-1;i>=0;i--){
+           if(st.empty())
+           st.push({t[i],i});
+           else if(!st.empty()){
+               while(!st.empty() && st.top().first<=t[i]){
+                   st.pop();
+               }
+
+               if(st.empty())
+               ret[i]=0;
+               else
+               ret[i]=st.top().second-i;
+           }
+
+           st.push({t[i],i});
+       }
+       return ret;
     }
 };
