7428a0ce9142c9695085999c1a89b3e24282fc2d
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-06-15 07:56:29 +0000

    Sync LeetCode submission - Find Duplicate Subtrees (cpp)

--- problems/find_duplicate_subtrees/solution.cpp
+++ problems/find_duplicate_subtrees/solution.cpp
@@ -11,29 +11,25 @@
  */
 class Solution {
 public:
+    using T=TreeNode*;
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        unordered_map<string, int> frequency;
-        vector<TreeNode*> result;
-        findDuplicates(root, frequency, result);
-        return result;
-    }
-    
-private:
-    string findDuplicates(TreeNode* node, unordered_map<string, int>& frequency, vector<TreeNode*>& result) {
-        if (!node) {
+        unordered_map<string,int>hash;
+        vector<T>have;
+
+        function<string(T)>recur=[&](T node)->string{
+            if(!node)
             return "#";
-        }
-        
-        string leftIdentifier = findDuplicates(node->left, frequency, result);
-        string rightIdentifier = findDuplicates(node->right, frequency, result);
-        // this thing simply means if two string having same identifier i.e. 4,#,# and 4,#,# it means we found two same subtree 
-        string identifier = to_string(node->val) + "," + leftIdentifier + "," + rightIdentifier;
-        frequency[identifier]++;
-        
-        if (frequency[identifier] == 2) {
-            result.push_back(node);
-        }
-        
-        return identifier;
+
+            auto L=recur(node->left);
+            auto R=recur(node->right);
+
+            auto s="*"+L+to_string(node->val)+R+"*";
+            if(++hash[s]==2)
+                have.push_back(node);
+            
+            return s;
+        };
+        recur(root);
+        return have;
     }
 };
