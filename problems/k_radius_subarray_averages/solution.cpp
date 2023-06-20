2047ab4f565aad6af3223a5e3a59d382d57fb9fe
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-06-20 04:54:20 +0000

    Sync LeetCode submission - K Radius Subarray Averages (cpp)

--- problems/k_radius_subarray_averages/solution.cpp
+++ problems/k_radius_subarray_averages/solution.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    vector<int> getAverages(vector<int>& nums, int k) {
-        int n;n=nums.size();
-        vector<int>res(n,-1);
+    vector<int> getAverages(vector<int>& A, int k) {
+        int n=A.size();
+
+        vector<int>ans(n,-1);
         if(n<2*k+1)
-        return res;
-        long long int sum=0,i,j=0;
-        for(i=0;i<(2*k+1);i++)
-        {
-            sum+=nums[i];
+        return ans;
+
+        long long curr=accumulate(A.begin(),A.begin()+2*k,0LL);
+        int j=0;
+        
+        for(int i=k;i<n;i++){
+            if(i<n-k){
+                curr+=A[2*k+j];
+                ans[i]=curr/(2*k+1);
+                curr-=A[j++];
+            }
         }
-        i--;
-        res[i-k]=sum/(2*k+1);
-        for(int r=k+1;i<n-1;r++,j++)
-        {
-            sum+=nums[++i]-nums[j];
-            res[r]=sum/(2*k+1);
-        }
-        return res;
+        return ans;
     }
 };
