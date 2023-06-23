b0748975bfcc9fea0e55254be2212757acee6832
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-06-23 05:37:54 +0000

    Sync LeetCode submission - Longest Arithmetic Subsequence (cpp)

--- problems/longest_arithmetic_subsequence/solution.cpp
+++ problems/longest_arithmetic_subsequence/solution.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int longestArithSeqLength(vector<int>& nums) {
-        int n,res=INT_MIN;
-        n=nums.size();
-        int dp[1001][1001]={0};
-    
-        for(int i=0;i<n;i++)
-        {
-          for(int j=0;j<i;j++)
-          {
-              int diff=nums[i]-nums[j]+500;
-              dp[i][diff]=dp[j][diff]>0?dp[j][diff]+1:2;
-              res=max(res,dp[i][diff]);
-          }
+    int longestArithSeqLength(vector<int>& A) {
+        int n=A.size();
+        int dp[1005][1005]={0};
+
+        int h=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                int diff=A[i]-A[j]+500;
+                dp[i][diff]=dp[j][diff]>0?dp[j][diff]+1:2;
+                h=max(h,dp[i][diff]);
+            }
         }
-        return res;
-        
+        return h;
     }
 };
