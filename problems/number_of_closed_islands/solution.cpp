177a2690b7da5cfed0dc0b0b48a630b75a9ae6be
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-07-03 11:37:47 +0000

    Sync LeetCode submission - Number of Closed Islands (cpp)

--- problems/number_of_closed_islands/solution.cpp
+++ problems/number_of_closed_islands/solution.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid,int x,int y){
-        int n,p;n=grid.size();
-        p=grid[0].size();
-        if(x<0 or y<0 or x>n-1 or y>p-1 or grid[x][y]==1)
-        return;
-        
-        
-        grid[x][y]=1;
-        dfs(grid,x+1,y);
-        dfs(grid,x,y-1);
-        dfs(grid,x-1,y);
-        dfs(grid,x,y+1);
-    }
     int closedIsland(vector<vector<int>>& grid) {
-        int n,p,cnt=0;n=grid.size();
-        p=grid[0].size();
+         int n=grid.size();
+    int p=grid[0].size();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<p;j++){
-                if((i==0 or i==n-1 or j==0 or j==p-1) and grid[i][j]==0){
-                    dfs(grid,i,j);
-                }
+    function<void(int,int)>dfs=[&](int x,int y){
+        if(x<0 || x>=n || y<0 || y>=p || grid[x][y])
+        return;
+
+        grid[x][y]=1;
+        dfs(x-1,y);
+        dfs(x+1,y);
+        dfs(x,y-1);
+        dfs(x,y+1);
+
+    };
+
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<p;j++){
+            if(grid[i][j]==0 and (i==n-1 || i==0 || j==0 || j==p-1)){
+                dfs(i,j);
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<p;j++){
-                if(grid[i][j]==0){
-                    cnt++;
-                    dfs(grid,i,j);
-                }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<p;j++){
+            if(grid[i][j]==0){
+                dfs(i,j);
+                cnt++;
             }
         }
-    
-
-        return cnt;
+    }
+    cout<<cnt<<endl;
+    return cnt;
+        
     }
 };
