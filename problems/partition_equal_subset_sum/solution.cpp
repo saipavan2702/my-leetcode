e2d5618cc1c9d9af3c50b86f72bff9949c24dffb
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-07-06 04:27:11 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (cpp)

--- problems/partition_equal_subset_sum/solution.cpp
+++ problems/partition_equal_subset_sum/solution.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int n;n=nums.size();
-    int sum=0;
-    sum=accumulate(nums.begin(),nums.end(),sum);
-    if(sum&1)
-    return 0;
-    cout<<sum;
-    sum/=2;
-  
-   bool dp[n+1][sum+1];  
-    for(int i=0;i<=n;i++)
-    dp[i][0]=1;
-    for(int i=1;i<=sum;i++)
-    dp[0][i]=0;
+    bool canPartition(vector<int>& A) {
+        int n=A.size();
+        int tot=accumulate(begin(A),end(A),0);
+        if(tot&1)return 0;
+        tot/=2;
 
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=sum;j++)
-        {
-           if(nums[i-1]<=j)
-           dp[i][j]=(dp[i-1][j-nums[i-1]] or dp[i-1][j]);
-           else
-           dp[i][j]=dp[i-1][j];
-            
-        }
-    }
-    return dp[n][sum];
+        vector<unordered_map<int,int>>dp(n+1);
+        function<bool(int,int)>recur=[&](int i, int tot)->bool{
+            if(tot==0)return 1;
+            if(i>=n || tot<0)return 0;
+            if(dp[i].find(tot)!=dp[i].end())
+            return dp[i][tot];
+
+            return dp[i][tot]=recur(i+1,tot-A[i]) or recur(i+1,tot);
+        };
+        return recur(0,tot);
     }
 };
