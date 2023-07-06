5d2ec05ec16287a37d597935e2e986eebed43257
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-07-06 11:49:17 +0000

    Sync LeetCode submission - Decode Ways (cpp)

--- problems/decode_ways/solution.cpp
+++ problems/decode_ways/solution.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int n;
-        n=s.size();
-        
-        int dp[105]={0};
-        dp[n]=1;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(s[i]=='0')
-            dp[i]=0;
-            else
-            {
-                dp[i]=dp[i+1];
-                if(s[i]=='1' or s[i]=='2' and s[i+1]<'7')
-                dp[i]+=dp[i+2];
-            }
-        }
-        return dp[0];
+        int n=s.size();
+        int dp[105];
+        memset(dp,-1,sizeof(dp));
+
+        function<int(int)>recur=[&](int i){
+            if(i>=n)return 1;
+            if(s[i]=='0') return 0;
+            if(dp[i]!=-1)return dp[i];
+
+            int res=0;
+            res+=recur(i+1);
+            if(i+1<n and (s[i]=='1' or (s[i]=='2' and s[i+1]<'7')))
+            res+=recur(i+2);
+
+            return dp[i]=res;
+        };
+        return recur(0);
     }
 };
