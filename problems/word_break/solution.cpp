f6b171a90cd57f3c7322af9c38565e9ee8a54ff9
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-07-06 07:25:55 +0000

    Sync LeetCode submission - Word Break (cpp)

--- problems/word_break/solution.cpp
+++ problems/word_break/solution.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
-    bool wordBreak(string s,vector<string>&word) {
-        int n;n=s.size();
-        unordered_set<string>dict;
-        for(auto x:word)
-        dict.insert(x);
-        
-        queue<int>q;
-        unordered_set<int>st;
-        q.push(0);
-        while(!q.empty())
-        {
-            int i=q.front();
-            q.pop();
-            if(st.find(i)==st.end())
-            {
-                st.insert(i);
-                for(int j=i;j<n;j++)
-                {
-                    string str=s.substr(i,j-i+1);
-                    if(dict.find(str)!=dict.end())
-                    {
-                        q.push(j+1);
-                        if(j==n-1)
-                        return 1;
-                    }
-                    
-                }
+    bool wordBreak(string s, vector<string>& D) {
+        int n=s.size();
+        unordered_set<string>st(D.begin(),D.end());
+        vector<int>dp(n+1,-1);
+
+        function<bool(int,string)>recur=[&](int x, string s)->bool{
+            int n=s.size();
+            if(x==n)    return 1;
+            if(dp[x]!=-1)return dp[x];
+
+            for(int i=x;i<n;i++){
+                if(recur(i+1,s) and st.find(s.substr(x,i+1-x))!=st.end())
+                return dp[x]=1;
             }
-        }
-        return 0;
-        
+            return dp[x]=false;
+        };
+        return recur(0,s);
     }
 };
