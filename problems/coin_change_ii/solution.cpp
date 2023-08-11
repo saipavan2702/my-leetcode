05768ffb5520e630ba37ea4346cb1bbdb30b0c8b
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-08-11 07:03:27 +0000

    Sync LeetCode submission - Coin Change II (cpp)

--- problems/coin_change_ii/solution.cpp
+++ problems/coin_change_ii/solution.cpp
@@ -3,16 +3,17 @@ public:
     int change(int need, vector<int>& coins) {
         int n=coins.size();
         int dp[305][5005];
-        memset(dp,-1,sizeof(dp));
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<=need;i++)dp[0][i]=(i%coins[0])==0;
 
-        function<int(int,int)>recur=[&](int i,int curr){
-            if(curr==need)return 1;
-            if(i>=n || curr>need)return 0;
-            if(dp[i][curr]!=-1)return dp[i][curr];
-
-            int ans=recur(i+1,curr)+recur(i,curr+coins[i]);
-            return dp[i][curr]=ans;
-        };
-        return recur(0,0);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=need;j++){
+                if(j>=coins[i])
+                dp[i][j]=dp[i-1][j]+dp[i][j-coins[i]];
+                else
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n-1][need];
     }
 };
