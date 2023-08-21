23687c06604721cd71ed8bde54c0a44bf612b518
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-08-21 05:15:33 +0000

    Sync LeetCode submission - Repeated Substring Pattern (cpp)

--- problems/repeated_substring_pattern/solution.cpp
+++ problems/repeated_substring_pattern/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-    int n;
-    n=s.size();
-    string a;
-    a=(s+s).substr(1,2*n-2);
-    return a.find(s)!=-1;
+        int n=s.size();
+        
+        string a;
+        a=(s+s).substr(1,2*n-2);
+        return a.find(s)!=-1;
     }
 };
