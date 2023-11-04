56b0d9ebd4f09943958a60f4891791c8ae31f056
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-11-04 09:40:14 +0000

    Sync LeetCode submission - Last Moment Before All Ants Fall Out of a Plank (cpp)

--- problems/last_moment_before_all_ants_fall_out_of_a_plank/solution.cpp
+++ problems/last_moment_before_all_ants_fall_out_of_a_plank/solution.cpp
@@ -3,8 +3,10 @@ public:
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
         sort(left.begin(),left.end(),greater<int>());
         sort(right.begin(),right.end());
+
         if(left.size()==0)
         return n-right[0];
+        
         if(right.size()==0)
         return left[0]-0;
         
